eb12f0bd68ddf079e5b0dfac6e411440cc2b40d2
Avni <avniprajapati21101@gmail.com>
2025-02-26 18:51:51 +0530

    imp: Increased the alpha value of the deep purple color in the rounded container.

--- lib/app/modules/home/widgets/rounded_container.dart
+++ lib/app/modules/home/widgets/rounded_container.dart
@@ -9,7 +9,7 @@ class RoundContainer extends StatelessWidget {
       height: 10,
       width: 10,
       decoration: BoxDecoration(
-        color: Colors.deepPurple.withAlpha(50),
+        color: Colors.deepPurple.withAlpha(70),
         borderRadius: BorderRadius.circular(70),
       ),
     );
